558ad318997845ee7f3e8678eb28e9e83ce6774a
Adam <adam.galtrey@gmail.com>
2020-02-02 22:40:33 +0000

    Animations for home page loading

--- pages/index.js
+++ pages/index.js
@@ -2,25 +2,11 @@ import "~/styles/index.scss";
 
 const Index = () => (
     <div id='page-wrapper'>
-        {/*<h1>This is a H1 Block</h1>*/}
-        {/*<h2>This is a H2 Block</h2>*/}
-        {/*<h3>This is a H3 Block</h3>*/}
-        {/*<p>This is a standard paragraph.</p>*/}
-        {/*<br/>*/}
-        {/*<h1 className='font-dark'>This is a H1 Block Dark</h1>*/}
-        {/*<h2 className='font-dark'>This is a H2 Block Dark</h2>*/}
-        {/*<h3 className='font-dark'>This is a H3 Block Dark</h3>*/}
-        {/*<p>This is a standard paragraph with dark font.</p>*/}
-        {/*<br/>*/}
-        {/*<h1 className='highlight-red'>This is a H1 Block Highlighted</h1>*/}
-        {/*<h2 className='highlight-red'>This is a H2 Block Highlighted</h2>*/}
-        {/*<h3 className='highlight-red'>This is a H3 Block Highlighted</h3>*/}
-        {/*<p>This is a <span className='highlight-red'>standard paragraph</span> with highlighted font.</p>*/}
 
         <div className='left-panel'>
-            <img className='head-shot' src='/headshot.png'/>
+            <img className='head-shot fade-in-anim' src='/headshot.png'/>
 
-            <div className='social-icons'>
+            <div className='social-icons fade-in-anim-2-left'>
                 <img src='/social/sm/github.png'/>
                 <img src='/social/sm/linkedin.png'/>
                 <img src='/social/sm/stackoverflow.png'/>
@@ -30,10 +16,19 @@ const Index = () => (
 
         <div className='main-panel'>
 
+            <nav className='fade-in-anim'>
+                <ul>
+                    <li>About</li>
+                    <li>Skills</li>
+                    <li>Experience</li>
+                    <li>Projects</li>
+                </ul>
+            </nav>
+
             <div className='page-panel'>
 
                 <div className='centered'>
-                    <div className='hero-text'>
+                    <div className='hero-text fade-in-anim-2'>
                         <h1 className='mb-30'>Hey, I'm <span className='highlight-red'>Adam Galtrey</span></h1>
                         <h3>
                             I’m a full stack developer based in London, UK with a passion for building high quality websites and
