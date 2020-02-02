d39c9230a3ef0d0c8c6d1dfc40d94844f7fc8e0c
Adam <adam.galtrey@gmail.com>
2020-02-02 12:53:20 +0000

    Add favicons as a circle

--- components/Head.js
+++ components/Head.js
@@ -9,7 +9,14 @@ const Head = () => (
         <meta charSet="UTF-8"/>
         <meta name="description" content={defaultDescription}/>
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5"/>
-            <link href="https://fonts.googleapis.com/css?family=Open+Sans|Roboto&display=swap" rel="stylesheet" />
+
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans|Roboto&display=swap" rel="stylesheet"/>
+
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png"/>
+        <link rel="manifest" href="/favicons/site.webmanifest"/>
+
         <title>{defaultTitle}</title>
     </NextHead>
 );
