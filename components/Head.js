79942f966604286c5d407d1af6a73a3fcdd9ef96
Adam <adam.galtrey@gmail.com>
2020-02-02 12:45:12 +0000

    Importing fonts and colour schemes.

--- components/Head.js
+++ components/Head.js
@@ -9,6 +9,7 @@ const Head = () => (
         <meta charSet="UTF-8"/>
         <meta name="description" content={defaultDescription}/>
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5"/>
+            <link href="https://fonts.googleapis.com/css?family=Open+Sans|Roboto&display=swap" rel="stylesheet" />
         <title>{defaultTitle}</title>
     </NextHead>
 );
--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,20 @@ import "~/styles/index.scss";
 
 const Index = () => (
     <div>
-        <p>Hello Next.js</p>
+        <h1>This is a H1 Block</h1>
+        <h2>This is a H2 Block</h2>
+        <h3>This is a H3 Block</h3>
+        <p>This is a standard paragraph.</p>
+        <br/>
+        <h1 className='font-dark'>This is a H1 Block Dark</h1>
+        <h2 className='font-dark'>This is a H2 Block Dark</h2>
+        <h3 className='font-dark'>This is a H3 Block Dark</h3>
+        <p>This is a standard paragraph with dark font.</p>
+        <br/>
+        <h1 className='highlight-red'>This is a H1 Block Highlighted</h1>
+        <h2 className='highlight-red'>This is a H2 Block Highlighted</h2>
+        <h3 className='highlight-red'>This is a H3 Block Highlighted</h3>
+        <p>This is a <span className='highligh-red'>standard paragraph</span> with dark font.</p>
     </div>
 );
 
