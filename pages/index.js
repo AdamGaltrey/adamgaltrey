0174ad47aa80b63ada4633f1ab57667d7962f8f6
Adam <adam.galtrey@gmail.com>
2020-02-08 12:24:56 +0000

    Update all mobile styling

--- pages/index.js
+++ pages/index.js
@@ -1,47 +1,76 @@
 import "~/styles/index.scss";
+import {useState} from 'react';
+import classNames from 'classnames';
 
-const Index = () => (
-    <div id='page-wrapper'>
+const Index = () => {
 
-        <div className='left-panel'>
-            <img className='head-shot fade-in-anim' src='/headshot.png'/>
+    const [menuOpen, setMenuOpen] = useState(false);
 
-            <div className='social-icons fade-in-anim-2-left'>
-                <img src='/social/sm/github.png'/>
-                <img src='/social/sm/linkedin.png'/>
-                <img src='/social/sm/stackoverflow.png'/>
-                <img src='/social/sm/instagram.png'/>
+    return (
+        <div id='page-wrapper'>
+
+            <div className='left-panel desktop-only'>
+                <img className='head-shot fade-in-anim' src='/headshot.png'/>
+
+                <div className='social-icons fade-in-anim-2-left'>
+                    <img className='social' src='/social/sm/github.png'/>
+                    <img className='social' src='/social/sm/linkedin.png'/>
+                    <img className='social' src='/social/sm/stackoverflow.png'/>
+                    <img className='social' src='/social/sm/instagram.png'/>
+                </div>
             </div>
-        </div>
 
-        <div className='main-panel'>
+            <div className='main-panel'>
 
-            <nav className='fade-in-anim'>
-                <ul>
-                    <li>About</li>
-                    <li>Skills</li>
-                    <li>Experience</li>
-                    <li>Projects</li>
-                </ul>
-            </nav>
+                <div className='nav-container'>
 
-            <div className='page-panel'>
+                    <img className='mobile-only head-shot-mobile fade-in-anim' src='/headshot.png'/>
 
-                <div className='centered'>
-                    <div className='hero-text fade-in-anim-2'>
-                        <h1 className='mb-30'>Hey, I'm <span className='highlight-red'>Adam Galtrey</span></h1>
-                        <h3>
-                            I’m a full stack developer based in London, UK with a passion for building high quality websites and
-                            applications.
-                        </h3>
+                    <button
+                        className={classNames("fade-in-anim burger-menu hamburger hamburger--spring mobile-only", {'is-active': menuOpen})}
+                        onClick={() => setMenuOpen(!menuOpen)}
+                        type="button"
+                    >
+                        <span className="hamburger-box">
+                            <span className="hamburger-inner"></span>
+                        </span>
+                    </button>
+
+                    <nav className='fade-in-anim-desktop-only' aria-expanded={menuOpen}>
+                        <ul>
+                            <li>About</li>
+                            <li>Skills</li>
+                            <li>Experience</li>
+                            <li>Projects</li>
+                        </ul>
+                    </nav>
+                </div>
+
+                <div className='page-panel first-panel'>
+
+                    <div className='centered'>
+                        <div className='hero-text fade-in-anim-2'>
+                            <h1 className='mb-30'>Hey, I'm <span className='highlight-red'>Adam Galtrey</span></h1>
+                            <h3>
+                                I’m a full stack developer based in London, UK with a passion for building high quality websites and
+                                applications.
+                            </h3>
+                        </div>
                     </div>
+
+                </div>
+
+                <div className='mobile-footer'>
+                    <img className='social' src='/social/sm/github.png'/>
+                    <img className='social' src='/social/sm/linkedin.png'/>
+                    <img className='social' src='/social/sm/stackoverflow.png'/>
+                    <img className='social' src='/social/sm/instagram.png'/>
                 </div>
 
             </div>
 
         </div>
-
-    </div>
-);
+    );
+}
 
 export default Index;
