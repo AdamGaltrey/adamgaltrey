d6ffe9a3a25a4836c981405d51f8831cd844fc45
Adam <adam.galtrey@gmail.com>
2020-02-08 22:25:02 +0000

    Social links point somewhere

--- pages/index.js
+++ pages/index.js
@@ -13,10 +13,23 @@ const Index = () => {
                 <img className='head-shot fade-in-anim' src='/headshot.png'/>
 
                 <div className='social-icons fade-in-anim-2-left'>
-                    <img className='social' src='/social/sm/github.png'/>
-                    <img className='social' src='/social/sm/linkedin.png'/>
-                    <img className='social' src='/social/sm/stackoverflow.png'/>
-                    <img className='social' src='/social/sm/instagram.png'/>
+                    <a href='mailto:adam.galtrey@gmail.com'>
+                        <img className='social' src='/social/sm/email.png'/>
+                    </a>
+
+                    <a href='https://github.com/AdamGaltrey' target='_blank' rel='noopener'>
+                        <img className='social' src='/social/sm/github.png'/>
+                    </a>
+
+                    <a href='https://www.linkedin.com/in/adam-galtrey-b48ab3109/' target='_blank' rel='noopener'>
+                        <img className='social' src='/social/sm/linkedin.png'/>
+                    </a>
+                    <a href='https://stackoverflow.com/users/5182868/adam' target='_blank' rel='noopener'>
+                        <img className='social' src='/social/sm/stackoverflow.png'/>
+                    </a>
+                    <a href='https://www.instagram.com/adamgaltrey/' target='_blank' rel='noopener'>
+                        <img className='social' src='/social/sm/instagram.png'/>
+                    </a>
                 </div>
             </div>
 
@@ -76,10 +89,23 @@ const Index = () => {
                 </div>
 
                 <div className='mobile-footer'>
-                    <img className='social' src='/social/sm/github.png'/>
-                    <img className='social' src='/social/sm/linkedin.png'/>
-                    <img className='social' src='/social/sm/stackoverflow.png'/>
-                    <img className='social' src='/social/sm/instagram.png'/>
+                    <a href='mailto:adam.galtrey@gmail.com'>
+                        <img className='social' src='/social/sm/email.png'/>
+                    </a>
+
+                    <a href='https://github.com/AdamGaltrey' target='_blank' rel='noopener'>
+                        <img className='social' src='/social/sm/github.png'/>
+                    </a>
+
+                    <a href='https://www.linkedin.com/in/adam-galtrey-b48ab3109/' target='_blank' rel='noopener'>
+                        <img className='social' src='/social/sm/linkedin.png'/>
+                    </a>
+                    <a href='https://stackoverflow.com/users/5182868/adam' target='_blank' rel='noopener'>
+                        <img className='social' src='/social/sm/stackoverflow.png'/>
+                    </a>
+                    <a href='https://www.instagram.com/adamgaltrey/' target='_blank' rel='noopener'>
+                        <img className='social' src='/social/sm/instagram.png'/>
+                    </a>
                 </div>
 
             </div>
